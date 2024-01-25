16702a7cf8fe1a52042bf047b51e99134644ef40
0xR3TR0 <oliwier@klefas.com>
2024-01-25 23:39:34 +0100

    1.0.0.1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Scripts and functions on this page are the creation of 0xR3TR0. All rights reserved.  
 // Selecting DOM elements
 const quoteText = document.querySelector(".quote");
 const quoteBtn = document.querySelector("button");
